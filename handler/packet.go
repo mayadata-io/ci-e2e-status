7edebad5cf9f25384a966892026720115c9b81ef
bhaskarjb <hcbhaskar7@gmail.com>
2019-08-23 18:39:15 +0530

    check for joblog_url
    Signed-off-by: bhaskarjb <hcbhaskar7@gmail.com>

--- handler/packet.go
+++ handler/packet.go
@@ -120,7 +120,9 @@ func PacketData(token, triggredIDColumnName, pipelineTableName, jobTableName str
 				sqlStatement := fmt.Sprintf("INSERT INTO %s (pipelineid, id, status, stage, name, ref, created_at, started_at, finished_at, job_log_url) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10)"+
 					"ON CONFLICT (id) DO UPDATE SET status = $3, stage = $4, name = $5, ref = $6, created_at = $7, started_at = $8, finished_at = $9, job_log_url = $10 RETURNING id;", jobTableName)
 				id := 0
+				if len(pipelineJobsdata) != 0 {
 				jobLogURL = Kibanaloglink(packetPipelineData.Sha, packetPipelineData.ID, packetPipelineData.Status, pipelineJobsdata[j].StartedAt, pipelineJobsdata[j].FinishedAt)
+				}
 				err = database.Db.QueryRow(sqlStatement,
 					packetPipelineData.ID,
 					pipelineJobsdata[j].ID,
