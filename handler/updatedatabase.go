023decda0f92375181692f45c8e96ad9b8f0108a
Bhaskar@007 <40464957+bhaskarhc@users.noreply.github.com>
2019-12-18 16:02:30 +0530

    add go routine to run fuctions parallely (#51)
    Signed-off-by: bhaskarhc <hcbhaskar7@gmail.com>

--- handler/updatedatabase.go
+++ handler/updatedatabase.go
@@ -22,10 +22,10 @@ func UpdateDatabase() {
 		columnName := "packet_" + k8sVersion + "_pid"
 		pipelineTable := "packet_pipeline_" + k8sVersion
 		jobTable := "packet_jobs_" + k8sVersion
-		PacketData(token, columnName, pipelineTable, jobTable)
+		go PacketData(token, columnName, pipelineTable, jobTable)
 	}
-	KonvoyData(token, "konvoy_pid", "konvoy_pipeline", "konvoy_jobs")
-	releaseBranch(token, "e2e-openshift", "release-branch", "release_pipeline_data", "release_jobs_data")
+	go KonvoyData(token, "konvoy_pid", "konvoy_pipeline", "konvoy_jobs")
+	go releaseBranch(token, "e2e-openshift", "release-branch", "release_pipeline_data", "release_jobs_data")
 
 	// loop will iterate at every 2nd minute and update the database
 	tick := time.Tick(2 * time.Minute)
@@ -35,10 +35,10 @@ func UpdateDatabase() {
 			columnName := "packet_" + k8sVersion + "_pid"
 			pipelineTable := "packet_pipeline_" + k8sVersion
 			jobTable := "packet_jobs_" + k8sVersion
-			PacketData(token, columnName, pipelineTable, jobTable)
+			go PacketData(token, columnName, pipelineTable, jobTable)
 		}
-		KonvoyData(token, "konvoy_pid", "konvoy_pipeline", "konvoy_jobs")
-		releaseBranch(token, "e2e-openshift", "release-branch", "release_pipeline_data", "release_jobs_data")
+		go KonvoyData(token, "konvoy_pid", "konvoy_pipeline", "konvoy_jobs")
+		go releaseBranch(token, "e2e-openshift", "release-branch", "release_pipeline_data", "release_jobs_data")
 
 	}
 }
